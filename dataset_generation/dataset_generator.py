667d3f3df93dca20ba367b61d06fee24faf7622b
khaledhesham2017 <khaledhesham2017@hotmail.com>
2019-04-27 19:54:11 +0200

    add merge image

--- dataset_generation/dataset_generator.py
+++ dataset_generation/dataset_generator.py
@@ -9,7 +9,7 @@ from PIL import ImageFont, ImageDraw, Image
 from os import listdir
 from os.path import isfile, join
 import xml.etree.ElementTree as ET
-
+import sys
 
 # recive the height and the width of the image
 def generate_random_point(height, width):
@@ -34,10 +34,11 @@ def generate_random_coordinates(height, width, start_point):
 def get_shuffle_serial(size):
     digits = "٠١٢٣٤٥‬٦٧‬٨٩‬‬"
     places = [0, 1, 2, 3, 4, 5, 7, 8, 10, 11]
-    new_digits = " "
+    new_digits = ""
     for index in range(7):
         i = random.choice(places)
-        new_digits = new_digits + digits[i]
+        new_digits = new_digits+" " + digits[i]
+
     return new_digits
 
 
@@ -48,18 +49,44 @@ def init():
     original_images = [join(original_images_path, f) for f in listdir(original_images_path) if
                        isfile(join(original_images_path, f))]
 
+def init_2():
+    global original_images_path
+    global original_images
+    global original_back_path
+    global original_back
+    original_images_path = "dataset/image"
+    original_back_path =  "orginal/back-ground"
+    original_images = [join(original_images_path, f) for f in listdir(original_images_path) if
+                       isfile(join(original_images_path, f))]
+    original_back = [join(original_back_path, f) for f in listdir(original_back_path) if
+                       isfile(join(original_back_path, f))]
 
 def generate_images(image_count):
+    init()
     index = 0
     while index < image_count:
         image = random.choice(original_images)
+
         print(image)
+        if image == 'orginal/test3.jpeg':
+            # third
+            start_point = (287, 420)
+            start_point_2 = (800, 65)
+        elif image == 'orginal/test2.jpeg':
+        # second
+         start_point = (290, 400)
+         start_point_2 = (800, 60)
+        elif image ==  'orginal/test1.jpeg':
+        # first image  dimantion
+         start_point = (385, 270)
+         start_point_2 = (760, 70)
+
         image = cv2.imread(image)
         height, width = image.shape[:2]
         #start_point = generate_random_point(height, width)
-        start_point = (385, 270)
-        start_point_2 = (760, 70)
-        image_area = height * width
+
+
+        #image_area = height * width
         serial_width, serial_height, serial_font_size = 21,27,70
         '''if serial_width == False:
             index = index - 1
@@ -67,10 +94,10 @@ def generate_images(image_count):
        # serial_width = serial_width * 0.7
         #end_point = (round(start_point[0] + serial_width),
                    #  round(start_point[1] - serial_height))
-        end_point = ((start_point[0] + serial_width) + 9, start_point[1] - serial_height)
-        end_point_2 = ((start_point_2[0] + serial_width) + 7, start_point_2[1] - serial_height)
-        serial_area = serial_height * serial_width
-        font = cv2.FONT_HERSHEY_SIMPLEX
+       # end_point = ((start_point[0] + serial_width) + 9, start_point[1] - serial_height)
+        #end_point_2 = ((start_point_2[0] + serial_width) + 7, start_point_2[1] - serial_height)
+        #serial_area = serial_height * serial_width
+        #font = cv2.FONT_HERSHEY_SIMPLEX
         print("Image Num #", str(index), "start at :", start_point)
         original_text = get_shuffle_serial(7)
         text = original_text
@@ -81,24 +108,27 @@ def generate_images(image_count):
         # cv2.circle(image,start_point,2, (0,0,255), -1)
         # cv2.circle(image,end_point, 2, (0,0,255), -1)
         # cv2.imwrite("test.png",image)
-        fontpath = "Mirza/Mirza-Bold.ttf"
+        fontpath = chose_font()
+        print(fontpath)
         font = ImageFont.truetype(fontpath, 45)
         img_pil = Image.fromarray(image)
         draw = ImageDraw.Draw(img_pil)
         d = (start_point[0], start_point[1] - serial_height)
         d2 = (start_point_2[0], start_point_2[1] - serial_height)
         draw.text(d, text, font=font, fill=(0, 0, 0))
+        #merge_image("orginal/back-ground/back-1.jpg","orginal/test1.jpeg")
 
         draw.text(d2, text, font=font, fill=(0, 0, 0))
         # draw.rectangle((start_point,end_point))
         image = np.array(img_pil)
-        cv2.imwrite("dataset/" + str(index) + ".png", image)
+        cv2.imwrite("dataset/image/" + str(index) + ".png", image)
         # cv2.imshow("test",image)
         # cv2.waitKey()
         # cv2.destroyAllWindows()
         index += 1
-
-
+def chose_font():
+  fonts = ['Mirza/Mirza-Bold.ttf','Mirza/Mirza-Medium.ttf','Mirza/Mirza-Regular.ttf',"Mirza/Mirza-SemiBold.ttf"]
+  return  random.choice(fonts)
 def generate_xml(index):
     annotation = ET.Element('annotation')
     folder = ET.SubElement(annotation, 'folder')
@@ -150,11 +180,51 @@ def generate_xml(index):
     myfile.write(mydata)
     index = index + 1
 
+def merge_image (back_ground_path ,image_path,saved_path ='/dataset'):
+    back_ground  = cv2.imread(back_ground_path,1)
+   # fin = open(back_ground_path)
+    #back_ground = Image.open(fin)
+    #width, height =  back_ground.size
 
+
+    #back_ground.paste(image_path)
+    #image = back_ground.Draw(image_path)
+    #image.imwrite(saved_path)
+    img = cv2.imread('img.png',1)
+    vis = cv2.add(back_ground,img)
+    cv2.imwrite('out.png', vis)
+
+def merge_image(back_name, image_name, index, outfname='dataset/final'):
+        back = Image.open(back_name)
+        image = Image.open(image_name)
+        prc = round(random.uniform(.9,1.3),2)
+        # resize logo
+        wsize = int(min(back.size[0], back.size[1])*prc)
+        wpercent = (wsize / float(image.size[0]))
+        hsize = int((float(image.size[1]) * float(wpercent)))
+
+        simage = image.resize((wsize, hsize))
+        mbox = back.getbbox()
+        sbox = simage.getbbox()
+
+        # right bottom corner
+        box = (mbox[1] - sbox[1], mbox[2] - sbox[2])
+        print(box)
+        back.paste(simage, box)
+        back.save(outfname +"/" + str(index)+".png")
+
+def  generate_data(image_count):
+    init_2()
+    index = 0
+    while index < image_count:
+        image = random.choice(original_images)
+        back   = random.choice(original_back)
+        merge_image(back, image,index)
+        index +=1
 def main():
-    init()
-    generate_images(1)
 
+    #generate_images(100)
+    generate_data(1)
 
 if __name__ == '__main__':
     main()
